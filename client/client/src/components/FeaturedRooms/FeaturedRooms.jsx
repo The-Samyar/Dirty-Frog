ae08b2061f0baba4aa04e786fdb40916129b0ae9
ramin709 <79803446+ramin709@users.noreply.github.com>
2022-05-01 17:09:40 +0430

    HomePage
    FeaturedRoom component completed and Stats component created

--- client/client/src/components/FeaturedRooms/FeaturedRooms.jsx
+++ client/client/src/components/FeaturedRooms/FeaturedRooms.jsx
@@ -1,41 +1,58 @@
 import React from 'react'
 import './FeaturedRooms.css'
-import {Carousel} from 'react-bootstrap'
-import IMG from '../images/cover2.jpg';
+import IMG1 from '../images/room1.jpg'
+import IMG2 from '../images/room2.jpg'
+import IMG3 from '../images/room3.jpg'
 
 
 const FeaturedRooms = () => {
     return (
         <section>
             <h3 className="sectionTitle">Featured Rooms</h3>
-             {/* <Carousel fade className="carousel" interval={1000}>
-                <Carousel.Item className="carouselItem">
-                    <img src={IMG} alt="featuredRoom1"/>
-                    <Carousel.Caption className="carousel-caption">
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
+            <div className="featuredRoomContainer">
+                
+                <div className="featuredRoom">
+                    <div className="imageSection">
+                        <img src={IMG1} alt="King Room" />
+                    </div>
+                    <div className="infoSection">
+                        <h4 className="infoSectionTitle">King Room</h4>
+                        <span className="cost">$140/Night</span>
+                        <p className="infoDescription">
+                            Far far away, behind the word mountains, far from the countries Vokalia
+                             and Consonantia, there live the blind texts.
+                        </p>
+                    </div>
+                </div>
 
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel> */} 
+                <div className="featuredRoom">
+                    <div className="imageSection">
+                        <img src={IMG2} alt="Deluxe Room" />
+                    </div>
+                    <div className="infoSection">
+                        <h4 className="infoSectionTitle">King Room</h4>
+                        <span className="cost">$140/Night</span>
+                        <p className="infoDescription">
+                            Far far away, behind the word mountains, far from the countries Vokalia
+                             and Consonantia, there live the blind texts.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="featuredRoom">
+                    <div className="imageSection">
+                        <img src={IMG3} alt="Suit Room" />
+                    </div>
+                    <div className="infoSection">
+                        <h4 className="infoSectionTitle">King Room</h4>
+                        <span className="cost">$140/Night</span>
+                        <p className="infoDescription">
+                            Far far away, behind the word mountains, far from the countries Vokalia
+                             and Consonantia, there live the blind texts.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
