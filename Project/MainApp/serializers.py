65d52b93e746ad57f155d586e4330e161c968e8d
Samyar <samyar1380@gmail.com>
2022-05-15 00:07:03 +0430

    Modify MainApp
    Add Homepage view function
    Create serializers for roomtype data, roompictures and reviews
    Add 'bed' field to roomtype and correct 'description' dictation
    Add dummy info for roomtype, users, usersinfo, roompictures models

--- Project/MainApp/serializers.py
+++ Project/MainApp/serializers.py
@@ -4,9 +4,16 @@ from . import models
 class RoomPictureSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.RoomPicture
-        field = "__all__"
+        fields = "__all__"
+
+class RoomTypeSerializer(serializers.ModelSerializer):
+    room_pictures = serializers.SlugRelatedField(many=True, read_only=True, slug_field="picture_address")
+
+    class Meta:
+        model = models.RoomType
+        fields = ['room_name', 'cost_per_day', 'size', 'capacity', 'booked_count', 'description', 'room_pictures']
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Review
-        field = "__all__"
+        fields = "__all__"
--- Project/MainApp/views.py
+++ Project/MainApp/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from rest_framework.response import Response
-from .serializers import RoomPictureSerializer, ReviewSerializer
+from rest_framework.decorators import api_view
+
+from .serializers import RoomTypeSerializer, ReviewSerializer
 from . import models
 
+@api_view(('GET','POST'))
 def Homepage(request):
     if request.method == 'GET':
-        review = models.Review.objects.all()
-        serialized = RoomPictureSerializer(review)
-        se = RoomPictureSerializer()
-        return Response(serialized.data)
+        # TODO
+        # customer count, positive ratings
+        room_types = models.RoomType.objects.all()
+        room_types_serialized = RoomTypeSerializer(room_types, many=True)
 
-    elif request.method == 'POST':
-
-        pass
+        reviews = models.Review.objects.all().order_by('rate')[:9]
+        reviews_serialized = ReviewSerializer(reviews, many=True)
+        for item in room_types_serialized.data:
+            item = dict(item)
+        for item in reviews_serialized.data:
+            item = dict(item)
+        json_response = [
+            {"room_types" : room_types_serialized.data},
+            {"reviews" : reviews_serialized.data}
+        ]
+        return Response(json_response)
