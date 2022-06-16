a08c4f92fbfefb2464ff214035ba3cebfcf6d7f9
Samyar <samyar1380@gmail.com>
2022-06-17 00:09:54 +0430

    Validation for form submittion
    Validations:
    - checkIn before checkOut
    - checkIn and checkOut before today's date
    - rooms between 1-6 inclusive
    - adults between 1-6 inclusive
    - children between 0-6 inclusive

--- Project/api/serializers.py
+++ Project/api/serializers.py
@@ -34,3 +34,41 @@ class HeaderFormSerializer(serializers.Serializer):
     rooms = serializers.IntegerField()
     adults = serializers.IntegerField()
     children = serializers.IntegerField()
+
+    def validate(self, data):
+        errors = {}
+        # checkIn before checkOut
+        if data['checkIn'] and data['checkOut']:
+            if data['checkIn'] > data['checkOut']:
+                errors["checkOut"] = "Submitted check out date must be after check in date"
+
+        # checkIn and checkOut before today's date
+        if data['checkIn'] and data['checkOut']:
+            
+            # today's date : %Y-%m-%d
+            today = datetime.now().date()
+
+            if data['checkIn'] < today:
+                errors['checkIn'] = "Submitted check in date must not be before today's date"
+            
+            if data['checkOut'] < today:
+                errors['checkOut'] = "Submitted check out date must not be before today's date"
+
+        # rooms between 1-6 inclusive
+        if data['rooms']:
+            if data['rooms'] > 6 or data['rooms'] < 1:
+                errors['rooms'] = "Submitted number of rooms must be between 1 to 6 (inclusive)"
+
+        # adults between 1-6 inclusive
+        if data['adults']:
+            if data['adults'] > 6 or data['adults'] < 1:
+                errors['adults'] = "Submitted number of adults must be between 1 to 6 (inclusive)"
+
+        # children between 0-6 inclusive
+        if data['children']:
+            if data['children'] > 6 or data['children'] < 0:
+                errors['children'] = "Submitted number of children must be between 0 to 6 (inclusive)"
+        if errors:
+            raise serializers.ValidationError(errors)
+
+        return data
--- Project/api/views.py
+++ Project/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from datetime import date
 from django.db.models import Sum, Q, Count
+from datetime import datetime
 
 from .serializers import *
 from . import models
