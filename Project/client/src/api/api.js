1ba952a2d120adda7cf23e991d08f5f8be7ade5b
ramin709 <79803446+ramin709@users.noreply.github.com>
2022-07-19 13:04:28 +0430

    Contact Us & bugs
    In ContactUs page:
    - whole the page completed
    
    In Sign component:
    - api call implemented
    
    In Table component:
    - check availability form added

--- Project/client/src/api/api.js
+++ Project/client/src/api/api.js
@@ -5,7 +5,8 @@ const API = axios.create({baseURL: 'http://localhost:8000/api'})
 export const fetchStats = () => API.get('/Stats/');
 export const fetchFeaturedRooms = () => API.get('/FeaturedRooms/');
 export const fetchTestimonials = () => API.get('/Testimonials/');
-export const fetchBookNow = (data) => API.post('/BookNow/' , data)
-export const getBookNow = () => API.get('/BookNow/')
-export const sendSignUpData = (data) => API.post('/SignUp/', data)
-export const sendSignInData = (data) => API.post('/SignIn/', data)
+export const fetchBookNow = (data) => API.post('/BookNow/' , data);
+export const getBookNow = () => API.get('/BookNow/');
+export const reserveBookNow = (data) => API.post('/BookNow/' , data);
+export const sendSignUpData = (data) => API.post('/SignUp/', data);
+export const sendSignInData = (data) => API.post('/SignIn/', data);
